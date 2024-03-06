ce82b9133a06150afeecc5e9b49afecd9da5fac8
Aziz <113197790+haquema@users.noreply.github.com>
2024-03-06 00:35:03 +0000

    implement payment via checkout session with Stripe

---
+++ app/MapElement.jsx
@@ -0,0 +1,7 @@
+import { Loader } from '@googlemaps/js-api-loader';
+
+const loader = new Loader({
+  apiKey: process.env.GMAPS_API,
+  version: ,
+
+});
---
+++ app/api/checkout/route.js
@@ -0,0 +1,23 @@
+import { redirect } from 'next/navigation';
+const stripe = require('stripe')(process.env.STRIPE_TEST_API)
+
+export async function POST() {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${process.env.SERVER_URL}/checkout/success`,
+      cancel_url: `${process.env.SERVER_URL}/checkout/cancel`,
+      line_items: [
+        {
+          // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
+          price: 'price_1OqydPJOEYgXXATz5QtWvRJo',
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+    });
+    
+    return Response.json({ message: 'success', checkoutUrl: session.url });
+  } catch (err) {
+    return Response.json({ message: 'fail', error: {err} });
+  }
+}
