bd1f6a7001f8fc86b37f1fd77111b08abe254069
Aziz <113197790+haquema@users.noreply.github.com>
2024-03-08 01:39:10 +0000

    create users personal profile route and add nav link to navbar

--- app/loginv2/page.js
+++
@@ -1,9 +0,0 @@
-export default function Login() {
-  return (
-    <div className="flex relative w-dvw h-dvh -m-8 justify-center">
-      <form className="absolute top-1/4 flex flex-col justify-center border border-black rounded-lg w-80 h-20">
-        <input className="focus: text-center bg-transparent self-center" type="email" placeholder="email" name='email' />
-      </form>
-    </div>
-  )
-}
---
+++ app/profile/[id]/page.js
@@ -0,0 +1,10 @@
+import { getServerSession } from "next-auth"
+
+const ProfilePage = () => {
+
+  return (
+    <div>ProfilePage</div>
+  )
+}
+
+export default ProfilePage
