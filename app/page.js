ffeffb63f2cf69aae6d6d1169a2a752f42df6dd2
Aziz <113197790+haquema@users.noreply.github.com>
2024-08-12 19:28:40 +0100

    added register button to event page

--- app/page.js
+++ app/page.js
@@ -1,10 +1,13 @@
 import Link from "next/link"
+import { HomeIcon } from "@heroicons/react/16/solid"
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center">
-      <h1 className="text-orange-700 font-bold text-5xl m-10 text-center">Leveraging the power of community to make homeschooling better!</h1>
-      <p className="w-max mt-36">List of <Link href="/vendors" className="text-blue-400 font-semibold">Vendors</Link></p>
+    <div className="flex flex-row justify-center">
+      <div className="flex flex-col items-center max-w-[650px]">
+        <h1 className="text-orange-700 font-bold text-5xl m-10 text-center">Homeschooling powered by community!</h1>
+        <Link href="/vendors" className="text-blue-400 font-semibold"><HomeIcon className="size-10 fill-red-500 hover:fill-red-700 hover:animate-bounce"/></Link>
+      </div>
     </div>
   )
 }
--- app/vendors/[vendorId]/[activityID]/page.js
+++ app/vendors/[vendorId]/[activityID]/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import poster from "../../../../public/profile-stock-1.jpg";
 import { CalendarDaysIcon, ClockIcon, CurrencyPoundIcon } from "@heroicons/react/24/outline"
+import { Button } from "@nextui-org/react";
 
 export default function EventPage() {
   const eventInfo = {
@@ -17,38 +18,41 @@ export default function EventPage() {
   }
 
   return (
-    <div className="flex flex-col items-center gap-1">
-      <h1 className="text-3xl font-bold m-4">{eventInfo.name}</h1>
-      <Image
-        className='self-center border border-stone-400 shadow shadow-stone-200 rounded-md'
-        src={eventInfo.image}
-        width={350}
-        height={"auto"}
-        alt="Poster of the event"
-        priority={true}
-      />
-      <div className="flex justify-evenly items-center my-2 space-x-4">
-        <div className="flex items-center space-x-1">
-          <CalendarDaysIcon className="size-5" />
-          <p className="text-sm font-bold">{eventInfo.day}</p>
+    <div className="flex justify-center">
+      <div className="flex flex-col max-w-[600px] items-center gap-4">
+        <h1 className="text-3xl font-bold m-4">{eventInfo.name}</h1>
+        <Image
+          className='self-center border border-stone-400 shadow shadow-stone-200 rounded-md'
+          src={eventInfo.image}
+          width={350}
+          height={"auto"}
+          alt="Poster of the event"
+          priority={true}
+        />
+        <div className="flex justify-evenly items-center my-2 space-x-4">
+          <div className="flex items-center space-x-1">
+            <CalendarDaysIcon className="size-5" />
+            <p className="text-sm font-bold">{eventInfo.day}</p>
+          </div>
+          <div className="flex items-center space-x-1">
+            <ClockIcon className="size-5" />
+            <p className="text-sm font-bold">{eventInfo.time}</p>
+          </div>
+          <div className="flex items-center space-x-1">
+            <CurrencyPoundIcon className="size-5" />
+            <p className="text-sm font-bold">{eventInfo.price}/session</p>
+          </div>
         </div>
-        <div className="flex items-center space-x-1">
-          <ClockIcon className="size-5" />
-          <p className="text-sm font-bold">{eventInfo.time}</p>
+        <div className=''>
+          <h2 className='w-full border-b border-black font-semibold text-base mb-2'>Description</h2>
+          <p className="text-sm">{eventInfo.description}</p>
         </div>
-        <div className="flex items-center space-x-1">
-          <CurrencyPoundIcon className="size-5" />
-          <p className="text-sm font-bold">{eventInfo.price}/session</p>
+        <div className='w-full'>
+          <h2 className='w-full border-b border-black font-semibold text-base mb-2'>Location</h2>
+          <p className="text-sm">{eventInfo.location}</p>
+          {/* <iframe className='border-2 border-stone-200 rounded-lg w-full' loading="lazy" allowFullScreen src={"https://www.google.com/maps/embed/v1/place?q=place_id:ChIJDwbWyTwddkgRAoz5yH-rCRk&key=" + process.env.GMAPS_API}></iframe> */}
         </div>
-      </div>
-      <div className=''>
-        <h2 className='w-full border-b border-black font-semibold text-base mb-2'>Description</h2>
-        <p className="text-sm">{eventInfo.description}</p>
-      </div>
-      <div className='w-full'>
-        <h2 className='w-full border-b border-black font-semibold text-base mb-2'>Location</h2>
-        <p className="text-sm">{eventInfo.location}</p>
-        {/* <iframe className='border-2 border-stone-200 rounded-lg w-full' loading="lazy" allowFullScreen src={"https://www.google.com/maps/embed/v1/place?q=place_id:ChIJDwbWyTwddkgRAoz5yH-rCRk&key=" + process.env.GMAPS_API}></iframe> */}
+        <Button size="sm" className="bg-red-500 text-white">Register</Button>
       </div>
     </div>
   )
